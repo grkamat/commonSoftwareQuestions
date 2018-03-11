e66186d3e6758cdbf9242e7793763ed46ffe405c
Gayatri Ravi Kamat <gkamat@andrew.cmu.edu>
2018-03-11 18:56:16 -0400

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * @author grkamat
+ *
+ */
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+
+}
